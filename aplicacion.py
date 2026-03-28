f907ef5651b76ccfd524615551a577da53b9c21a
Jossi028 <acostajossie63@gmail.com>
2026-03-28 15:26:15 -0400

    Arreglo y usar GPU O CPU

--- aplicacion.py
+++ aplicacion.py
@@ -1,46 +1,49 @@
 # Nombre: Jossie Gabriel Acosta Ruiz
 # Matrícula: 24-EISN-2-029
 
-#Importamos las liberias y modelos que usamos
+# Importamos las librerías y modelos que usamos
 import gradio as gr
 from diffusers import StableDiffusionPipeline
 import torch 
 
-
-# descargamos y cargamos el Stable DIfussion
+# Descargamos y cargamos el Stable Diffusion
 pipe = StableDiffusionPipeline.from_pretrained(
-    "runwayml/stable-diffusion-v1-5"
+    "runwayml/stable-diffusion-v1-5",
+    torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32
 )
 
-# Por si no tenemos grafica
-pipe = pipe.to("cpu")
+# Usar GPU si no CPU
+device = "cuda" if torch.cuda.is_available() else "cpu"
+pipe = pipe.to(device)
 
 # Función para generar la imagen a partir del texto
 def generar_imagen(prompt):
-    image = pipe(prompt).images[0]
+    if device == "cuda":
+        with torch.amp.autocast("cuda"):
+            image = pipe(prompt, num_inference_steps=20).images[0]
+    else:
+        image = pipe(prompt, num_inference_steps=20).images[0]
     return image
 
-#Creacion de la interfaz visual
-
+# Creación de la interfaz visual
 with gr.Blocks() as demo:
-    gr.Markdown("#Generador de Fondos Inteligente")
+    gr.Markdown("# Generador de Fondos Inteligente")
 
     prompt_input = gr.Textbox(
         label="Describe tu fondo",
         placeholder="Ej: paisaje futurista azul"
     )
 
-#boton de generar la imagen
+    # Botón de generar la imagen
     generar_btn = gr.Button("Generar")
 
-#area donde se muestra la imagen
+    # Área donde se muestra la imagen
     output_image = gr.Image(label="Imagen generada")
 
     generar_btn.click(
-        fn=generar_imagen, #funcion que crea la imagen
-        inputs=prompt_input,#entrada del texto
-        outputs=output_image #salida de la imagen generada
+        fn=generar_imagen,   # función que crea la imagen
+        inputs=prompt_input, # entrada del texto
+        outputs=output_image # salida de la imagen generada
     )
 
 demo.launch()
-
